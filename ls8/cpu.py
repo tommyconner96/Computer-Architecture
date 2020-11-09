19d3ec81ad0a989927f8698ffaa167d2e53aa5ae
unknown <tommyconner96@gmail.com>
2020-11-08 22:23:44 -0500

    CALL and RET done day 4

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -6,13 +6,18 @@ import sys
 # HLT = 0b00000001
 # PRN = 0b01000111
 # MUL = 0b10100010
-SP = 7  # stack pointer
+# SP = 7  # stack pointer
 LDI = 130
 HLT = 1
 PRN = 71
+ADD = 160
+SUB = 161
 MUL = 162
+DIV = 163
 PUSH = 69
 POP = 70
+CALL = 80
+RET = 17
 
 
 class CPU:
@@ -26,6 +31,7 @@ class CPU:
         * `MDR`: Memory Data Register, holds the value to write or the value just read
         * `FL`: Flags, see below
         """
+        self.SP = 7
         self.pc = 0
         self.mar = 0
         self.mdr = 0
@@ -35,6 +41,7 @@ class CPU:
         # self.ie = None
         self.reg = [0] * 8
         self.ram = [0] * 256
+        self.reg[self.SP] = 256
 
     def load(self):
         """Load a program into memory."""
@@ -67,28 +74,21 @@ class CPU:
                 command = line_split[0].strip()
                 if command == '':
                     continue
-                # specify that the number is base 10
                 command_num = int(command, 2)
-                # print(command_num)
-                # program.append(command_num)
                 self.ram[address] = command_num
-                address +=1
-
-        # REVISIT IF WE GET AN INFINITE LOOP. DAY 3 lecture  1:10 for reference
-            # for instruction in program:
-            #     self.ram[address] = instruction
-            #     address += 1
-            # print(program)
-            # return program
-    # def halt(self):
-    #     self.running = False
+                address += 1
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        # elif op == "SUB": etc
+        elif op == "SUB":
+            self.reg[reg_a] -= self.reg[reg_b]
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
+        elif op == "DIV":
+            self.reg[reg_a] //= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -122,16 +122,35 @@ class CPU:
     def ram_write(self, val, adr):
         self.ram[adr] = val
 
+    def ALU_OPS(self, command):
+        # ADD = 160
+        # SUB = 161
+        # MUL = 162
+        # DIV = 163
+        if command == 160:
+            return "ADD"
+        if command == 161:
+            return "SUB"
+        if command == 162:
+            return "MUL"
+        if command == 163:
+            return "DIV"
+
     def run(self):
         running = self.running
         running = True
-
         while running:
             command = self.ram_read(self.pc)
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
-            # print(operand_a, operand_b)
-            # print(command)
+            # we will determine if operation is an ALU operation
+            is_alu = (command >> 5) & 0b001 == 1
+            if is_alu:
+                op_type = self.ALU_OPS(command)
+                # print(command)
+                self.alu(op_type, operand_a, operand_b)
+                self.pc += 3
+
             if command == HLT:
                 running = False
                 self.pc += 1
@@ -139,24 +158,33 @@ class CPU:
                 print(self.reg[operand_a])
                 self.pc += 2
             elif command == LDI:
-                # print(operand_a, operand_b)
                 self.reg[operand_a] = operand_b
-                self.pc += 3
-            elif command == MUL:
-                self.reg[operand_a] *= self.reg[operand_b]
                 self.pc += 3
             elif command == PUSH:
                 value_to_push = self.reg[operand_a]
                 # move the stack pointer down
-                self.reg[SP] -= 1
+                self.reg[self.SP] -= 1
                 # write the value to push, into the top of stack
-                self.ram[self.reg[SP]] = value_to_push
+                self.ram[self.reg[self.SP]] = value_to_push
                 self.pc += 2
             elif command == POP:
                 # Read the given register address
                 # Read the value at the top of the stack
                 # store that into the register given
-                self.reg[operand_a] = self.ram[self.reg[SP]]
+                self.reg[operand_a] = self.ram[self.reg[self.SP]]
                 # move the stack pointer back up
-                self.reg[SP] += 1
+                self.reg[self.SP] += 1
                 self.pc += 2
+            elif command == CALL:
+                # Push the return address onto the stack
+                # Move the SP down
+                self.reg[self.SP] -= 1
+                # Write the value of the next line to return to in the code
+                self.ram[self.reg[self.SP]] = self.pc + 2
+                # Set the PC to whatever is given to us in the register
+                self.pc = self.reg[operand_a]
+
+            elif command == RET:
+                # Pop the top of the stack and set the PC to the value of what was popped
+                self.pc = self.ram[self.reg[self.SP]]
+                self.reg[self.SP] += 1
